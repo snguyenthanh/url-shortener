560f16a8f8a4139c5d407c37088b5eed01ea6c60
unknown <thanhson16198@gmail.com>
2019-06-23 21:15:49 +0800

    Re-enable app.run

--- app.py
+++ app.py
@@ -2,7 +2,4 @@ from url_shortener import create_application
 
 if __name__ == "__main__":
     app = create_application()
-    for handler, (rule, router) in app.router.routes_names.items():
-        print('URL: ', rule)
-    print()
-    # app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
