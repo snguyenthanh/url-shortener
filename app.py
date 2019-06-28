d1c0ce5508c4eee445bfb31b1f9af3f1017ca14f
unknown <thanhson16198@gmail.com>
2019-06-28 08:40:15 +0800

    temporarily add db integration

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from url_shortener import create_application
 
-if __name__ == "__main__":
-    app = create_application()
-    app.run(host="0.0.0.0", port=8000, debug=True)
+app = create_application()
+
+@app.listener('after_server_start')
+async def connect_to_db():
+    await app.db.connect()
+
+@app.listener('after_server_stop')
+async def disconnect_from_db():
+    await app.db.disconnect()
+
+app.run(host="0.0.0.0", port=8000, debug=True)
--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,13 +1,28 @@
+from os import environ
+
 from sanic import Blueprint, Sanic
 
+from aiomysql import create_pool
 from url_shortener.accounts.urls import blueprint as account
 from url_shortener.urls import blueprint as root
 
 blueprints = Blueprint.group(root, account)
 
+async def setup_db():
+    pool = await create_pool(
+        host=os.environ.get('DATABASE_URL', '127.0.0.1'),
+        port=os.environ.get('DATABASE_PORT', 3306),
+        user=environ['DATABASE_USERNAME'],
+        password=environ['DATABASE_PASSWORD'],
+        db=environ['DATABASE_INSTANCE']
+    )
+    return pool
+
+
 def create_application():
     app = Sanic(__name__)
     app.blueprint(blueprints)
+	app.db = setup_db()
 
     return app
 
